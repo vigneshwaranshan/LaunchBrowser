45fc10e76884964cd47ba1c1105421991b2ebe2e
unknown <vigneshwarnanshanmugavel@gmail.com>
2021-02-01 17:13:08 +0530

    demo

---
+++ src/Launch/chrome.java
@@ -0,0 +1,14 @@
+package Launch;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class chrome {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hariharan\\eclipse-workspace\\Launchbrowser\\driver\\chromedriver.exe");
+   WebDriver driver = new ChromeDriver();
+   
+	
+	
+}
+}
